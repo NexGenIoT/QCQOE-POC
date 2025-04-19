88ef66ed6fc2b6195b3349d39cd178ac6ed9b8ad
Rampratapnexgts <rampratap@nexgts.com>
2025-04-19 13:11:56 +0530

    playback session registered with sdk currently api failing

--- src/Player.js
+++ src/Player.js
@@ -30,14 +30,17 @@ const BitmovinPlayer = () => {
     });
 
     const source = {
-      dash:
-        "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd",
-      hls:
-        "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8",
-      progressive:
-        "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/MI201109210084_mpeg-4_hd_high_1080p25_10mbits.mp4",
-      poster: "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/poster.jpg"
+      dash: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd',
+      hls: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8',
+      poster: 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/poster.jpg'
     };
+
+    const assetInfo = {
+      AssetID: "1235",
+      Provider: "Hotstar",
+      CDN: "AKAMAI"
+    }
+    analyticsSDK.registerPlaybackSession(assetInfo, player);
 
     player.load(source).then(() => {
       console.log('Player loaded successfully');
